1dd79791fc9e8833f3c3b3dcac7ae4962121f0f2
rohailtaha <taharohail77@gmail.com>
2024-09-29 19:05:47 +0500

    feat: build home page
    Following sections are made:
    - main/hero
    - projects
    - about
    - experience/education
    - skills

---
+++ src/components/SectionHeading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SectionHeading({ children }) {
+  return <h2 className="text-2xl font-bold">{children}</h2>;
+}
+
+export default SectionHeading;
--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,14 +1,256 @@
 import React from 'react';
-import Name from './Name';
-import Age from './Age';
 import '../../styles/globals.css';
+import { HEADER_HEIGHT } from '../../utils/constants';
+import SectionHeading from '../../components/SectionHeading';
+
+const PROJECTS = [
+  {
+    name: 'Startup, Inc',
+    skills: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    year: 2023,
+  },
+  {
+    name: 'Startup, Inc',
+    skills: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    year: 2023,
+  },
+  {
+    name: 'Startup, Inc',
+    skills: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    year: 2023,
+  },
+  {
+    name: 'Startup, Inc',
+    skills: ['Next.js', 'TailwindCSS', 'MongoDB'],
+    year: 2023,
+  },
+];
+
+const EXPERIENCES = [
+  {
+    company: 'JFreaks Software Sol.',
+    title: 'FullStack Developer',
+    start: 'Feb 2024',
+    end: 'Now',
+  },
+  {
+    company: 'ThinkSoft',
+    title: 'FullStack Developer',
+    start: 'Jan 2022',
+    end: 'Jan 2024',
+  },
+];
+
+const EDUCATION = [
+  {
+    institution: 'National University of Sciences and Technology',
+    degree: 'BSCS',
+    start: '2019',
+    end: '2023',
+  },
+  {
+    institution: 'Madina College',
+    degree: 'Intermediate',
+    start: '2017',
+    end: '2019',
+  },
+  {
+    institution: 'Allied Schools',
+    degree: 'Matriculation',
+    start: '2015',
+    end: '2017',
+  },
+];
+
+const SKILLS = {
+  frontend: [
+    'Next.js',
+    'React.js',
+    'TailwindCSS',
+    'HTML/CSS',
+    'JavaScript',
+    'Typescript',
+  ],
+  backend: [
+    'Node.js',
+    'Express.js',
+    'PostgreSQL',
+    'MongoDB',
+    'MySQL',
+    'Firebase',
+  ],
+  other: ['Docker', 'Figma'],
+};
 
 function App() {
   return (
     <div>
-      <h1 className="text-blue-500">What nice way</h1>
-      <Name />
-      <Age />
+      <header className="fixed w-full py-6 bg-white responsive-pad">
+        <nav className="mx-auto font-medium max-content-w flex-c gap-x-16 text-text-primary">
+          <a href="/" className="text-lg font-bold text-primary">
+            Rohail Taha
+          </a>
+          <div className="flex-c gap-x-8">
+            <a href="/">Work</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+          </div>
+        </nav>
+      </header>
+      <main style={{ paddingTop: HEADER_HEIGHT }} className="responsive-pad">
+        <section className="py-16 mx-auto max-content-w">
+          <div className="max-w-xl">
+            <h1 className="text-4xl font-bold">
+              Creating functional software for various ambitious purposes.
+            </h1>
+            <p className="mt-5 text-xl">
+              I&apos;m a fullstack web developer located in Pakistan.
+            </p>
+          </div>
+        </section>
+        <section className="py-16 mx-auto max-content-w">
+          <div className="flex-cb">
+            <SectionHeading>Selected Work</SectionHeading>
+            <div>
+              <a
+                href="/work"
+                className="px-4 py-2 font-medium border-2 rounded-full border-text-primary text-text-primary"
+              >
+                <span className="text-sm"> Show all projects</span>
+              </a>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-x-7 gap-y-9 mt-7">
+            {PROJECTS.map(project => {
+              return (
+                <div className="space-y-4" key={project.name}>
+                  <div className="bg-primary rounded-xl h-[230px]"></div>
+                  <div className="space-y-2">
+                    <h3 className="text-lg font-semibold">{project.name}</h3>
+                    <div className="text-sm flex-cb">
+                      <ul className="flex-c gap-x-2">
+                        {project.skills.map(skill => (
+                          <li key={skill}>{skill}</li>
+                        ))}
+                      </ul>
+                      <span className="px-2 py-0.5 font-medium bg-purple-300 rounded-full text-primary">
+                        {project.year}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        <section className="py-16 mx-auto max-content-w">
+          <SectionHeading>Bio</SectionHeading>
+          <div className="grid justify-between grid-cols-[1.2fr_1fr] mt-7 gap-x-24">
+            <div>
+              <div className="space-y-3 text-text-primary">
+                <p>
+                  For the past few years, I've been working with various
+                  startups, companies, and agencies.
+                </p>
+                <p>
+                  I love collaborating with individuals to create fantastic
+                  digital products while leveraging the latest technologies.
+                </p>
+                <p>
+                  In my free time, you'll usually find me building side projects
+                  and tackling frontend challenges to enhance my web development
+                  skills.
+                </p>
+              </div>
+              <a
+                href="/"
+                className="inline-block px-5 py-3 font-semibold text-white rounded-full mt-7 bg-primary"
+              >
+                <span>Download my resume</span>
+                <span></span>{' '}
+              </a>
+            </div>
+            <div>
+              <div className="px-2 pt-2 text-center -rotate-[6deg] -translate-y-10 bg-white border shadow-lg w-fit">
+                <img
+                  src="/me.png"
+                  className="bg-white w-[219px] h-[306px]"
+                ></img>
+                <span className="inline-block my-1 italic">This is me!</span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-16 mx-auto max-content-w">
+          <div className="grid lg:grid-cols-2 gap-x-16 gap-y-10">
+            <div>
+              <SectionHeading>Work Experience</SectionHeading>
+              <ul className="mt-10 space-y-6">
+                {EXPERIENCES.map(experience => {
+                  return (
+                    <li className="pb-6 border-b" key={experience.company}>
+                      <h3 className="text-lg font-bold">{experience.title}</h3>
+                      <div className="text-sm font-medium flex-cb gap-x-4">
+                        <span>{experience.company}</span>
+                        <span>
+                          {experience.start} - {experience.end}
+                        </span>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <SectionHeading>Education</SectionHeading>
+              <ul className="mt-10 space-y-6">
+                {EDUCATION.map(education => {
+                  return (
+                    <li className="pb-6 border-b" key={education.institution}>
+                      <h3 className="text-lg font-bold">{education.degree}</h3>
+                      <div className="text-sm font-medium flex-cb gap-x-4">
+                        <span>{education.institution}</span>
+                        <span className="text-nowrap">
+                          {education.start} - {education.end}
+                        </span>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section className="py-16 mx-auto max-content-w">
+          <SectionHeading>Tech Stack</SectionHeading>
+          <div className="grid grid-cols-3 font-medium gap-x-16 mt-7 text-text-primary">
+            <div>
+              <h3 className="text-text-secondary">Frontend</h3>
+              <ul className="mt-4 flex flex-col gap-y-4 flex-wrap max-h-[250px]">
+                {SKILLS.frontend.map(skill => {
+                  return <li key={skill}>{skill}</li>;
+                })}
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-text-secondary">Backend</h3>
+              <ul className="mt-4 flex flex-col gap-y-4 flex-wrap max-h-[250px]">
+                {SKILLS.backend.map(skill => {
+                  return <li key={skill}>{skill}</li>;
+                })}
+              </ul>
+            </div>
+            <div>
+              <h3 className="text-text-secondary">Other</h3>
+              <ul className="mt-4 flex flex-col gap-y-4 flex-wrap max-h-[250px]">
+                {SKILLS.other.map(skill => {
+                  return <li key={skill}>{skill}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
