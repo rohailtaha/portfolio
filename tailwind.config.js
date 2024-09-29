24389037c19db2fdbfa179d9d6b16d32b6845ebe
rohailtaha <taharohail77@gmail.com>
2024-09-29 18:29:11 +0500

    chore: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#6500B4',
+        'text-primary': '#000000',
+        'text-secondary': '#606060',
+      },
+      screens: {
+        xs: '420px',
+      },
+    },
   },
   plugins: [],
 };
