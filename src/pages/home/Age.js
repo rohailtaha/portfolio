ef4990962d4edc2af3922be4efc4517eb562c281
rohailtaha <taharohail77@gmail.com>
2024-09-29 18:28:49 +0500

    refactor: remove unused files

--- src/pages/home/Age.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Age() {
-  return <div>My Age is 18. Nice</div>;
-}
-
-export default Age;
--- src/pages/home/Name.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Name() {
-  return <div>My name is Rohail</div>;
-}
-
-export default Name;
